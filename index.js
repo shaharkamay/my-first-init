c6ca68fe5f311ecf3d9ae9724acf9b9c8276fa9b
shaharKamay <shahar.kamay@gmail.com>
2021-10-20 03:49:43 +0300

    added daysInMonth function

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function timeToNow(date){
 console.log(timeToNow([2021, 10, 8]));
 
 function timeFromAToB(date1 , date2) {
-    return moment(date1).to(date2)
+    return moment(date1).to(date2);
 }
 console.log(timeFromAToB([2021, 10, 8], [2021, 10, 20]));
+
+function daysInMonth(date){
+    return moment(date).daysInMonth();
+}
+console.log(daysInMonth("2024-02"));
