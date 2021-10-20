116a1dfd89b919dfe337a324b2172ca2e95be528
shaharKamay <shahar.kamay@gmail.com>
2021-10-20 03:44:05 +0300

    added timeToNow function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-console.log("hello world");
-//ll
-console.log("hellow again");
+//The function will get a date (by array) and return the Time from that date to now.
+const moment = require("moment")
+
+function timeToNow(date){
+    return moment(date).toNow(); 
+}
+console.log(timeToNow([2021, 10, 8]));
